5073d76cde6c34407759b7ff9fdad46aebdd7b78
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-04-28 20:32:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,19 @@ class Song
   attr_accessor :name :artist :genre
   @@artists = []
   @@count = 0
-  @@genres = {}
+  @@genres = []
   @@artist_count = {}
-  def initialize(song)
-    @song = song
+  def initialize(name, artist, genre)
+    @name = name
     @@count += 1
+    @@genres << genre
   end
   
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres
+  end
 end
